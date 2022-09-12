323516ca480ca5cc9cfae8fe1018438865b9a900
nicholas <nicholas.daly.2020@strath.ac.uk>
2022-09-12 17:01:11 +0100

    saved state

--- src/Card.js
+++ src/Card.js
@@ -1,44 +1,91 @@
-import React, { Component } from 'react';
-import uuidv4 from '../node_modules/uuid/dist/v4';
+import React, { useEffect, useState } from 'react'
 
-const TodoList = ['sleep'];
+
+const TodoList = [];
+
+ let counter = 0;
 
 export default function Card() {
-  const [todo, setTodo] = React.useState(TodoList);
   
+  const [todo, setTodo] = useState(TodoList);
+ 
+  
+  useEffect(() => {
+    const data = localStorage.getItem("myTodo");
+    if(data){
+      setTodo(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("myTodo", JSON.stringify(todo));
+  });
+ 
   const addTodo = () => {
     const input = document.getElementById("input");
-    if(input.value == ''){
+    if(input.value === ''){
       return;
+    } else {
+      if(todo.length === 0){
+        counter = +localStorage.getItem("counter");
+        setTodo([{id:counter, name:input.value,checked:false}]);
+        input.value = '';
+      } else {
+        counter = +localStorage.getItem("counter");
+        setTodo([...todo, {id:counter, name:input.value,checked:false}]);
+        input.value = '';
+      }
+      let count = counter;
+      count++;
+      localStorage.setItem("counter", count);
     }
-    setTodo([...todo, input.value]);
-    input.value = '';
   }
 
   const clearTodo = () => {
     setTodo([]);
+    counter=0;
+    localStorage.setItem("counter", counter);
   }
 
-    return (
-      <div className="card">
-        <h1>To-do List</h1>
-        <div className="todoList">
+  function handleInputChange(event, todos){
+    todos.checked = !todos.checked;
+    event.target.checked = todos.checked;
+    let num = 0;
+    //console.log(todos);
+    todo.forEach((element) => {
+      if(element.id === todos.id){
+        todo.splice(num, 1);
+      }
+      num ++;
+    })
+    //console.log(todo);
+    todo.splice(0, 0, todos);
+    let newArray = todo.map(todos => ({...todos}))
+    console.log(newArray);
+    setTodo(newArray);
+  }    
 
-          {todo.map((todo) => {
-            return (
-              <div className="todo__item" key={uuidv4()}>
-                  <input type='checkbox' className='todo__item-check'></input>
-                  <h3 className='todo__item-name'>{todo}</h3>
-              </div>
-            );
-          })}
+  return (
+    <div className="card">
+      <h1>To-do List</h1>
+      <div className="todoList">
 
-        </div>
-        <input type='text' id='input'></input>
-        <button onClick={addTodo}>Add to-do</button>
-        <button onClick={clearTodo}>Clear</button>
+      {todo.map((todos) => {
+        const {id, name, checked} = todos;
+        return ( 
+            <div key={id} className="todo__item">
+                <input type='checkbox' id={id} onChange={event => handleInputChange(event,todos)} checked={checked} className='todo__item-check'></input>
+                <h3 className='todo__item-name'>{name}</h3>
+            </div>
+          );
+      })}
+        
       </div>
-    )
+      <input type='text' id='input'></input>
+      <button onClick={addTodo}>Add to-do</button>
+      <button onClick={clearTodo}>Clear All</button>
+    </div>
+  );
 }
 
 
